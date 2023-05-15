62b77f2b94d584437cb068cfc8f10e7202073db1
Gabriela1719 <gabi.vidovic99@hotmail.com>
2023-05-15 10:39:21 +0200

    Enabling cors policy.

--- src/main/java/com/example/directory/config/SecurityConfig.java
+++ src/main/java/com/example/directory/config/SecurityConfig.java
@@ -3,10 +3,13 @@ package com.example.directory.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.lang.NonNull;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class SecurityConfig {
@@ -39,6 +42,16 @@ public class SecurityConfig {
                 .and()
                 .sessionFixation().newSession();
         return http.build();
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(@NonNull CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("http://localhost:3000");
+            }
+        };
     }
 
     @Bean
